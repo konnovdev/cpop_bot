eabc9b49949aabd60ccc3a26a0416904458af309
konnovdev <konnovdev@gmail.com>
2020-06-07 19:12:14 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.ACCESS_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sticker = open('static/hello_animated_sticker.tgs', 'rb')
+    bot.send_sticker(message.chat.id, sticker)
+    bot.send_message(message.chat.id, "Sup, {0.first_name}\n I am <b>{1.first_name}</b> a cpop fan"
+                     .format(message.from_user, bot.get_me()),
+                     parse_mode='html')
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
