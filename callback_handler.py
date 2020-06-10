587d6ef6585da6160bdc8f54f65f73fe6741b120
konnovdev <konnovdev@gmail.com>
2020-06-10 18:02:15 +0700

    Added blocking people when they enter the chat until they click the button

--- callback_handler.py
+++ callback_handler.py
@@ -1,5 +1,5 @@
 import logging
-from aiogram import types
+from aiogram import types, Bot
 
 from constants import CAPTCHA_SUCCESS
 
@@ -11,8 +11,13 @@ class CallbackHandler:
         if callback.data == CAPTCHA_SUCCESS:
             await self.__handleCaptchaPassed(bot, callback)
 
-    async def __handleCaptchaPassed(self, bot, callback: types.CallbackQuery):
+    async def __handleCaptchaPassed(self, bot: Bot, callback: types.CallbackQuery):
         await bot.edit_message_text(chat_id=callback.message.chat.id,
                                     message_id=callback.message.message_id,
                                     text="Congrats, " + callback.from_user.first_name + ", captcha passed",
                                     reply_markup=None)
+        await bot.restrict_chat_member(callback.message.chat.id,
+                                       callback.from_user.id,
+                                       can_send_messages=True,
+                                       can_send_media_messages=True,
+                                       can_send_other_messages=True)
--- new_members_handler.py
+++ new_members_handler.py
@@ -9,16 +9,25 @@ logger = logging.getLogger(__name__)
 
 class NewMembersHandler:
     async def handle(self, bot: Bot, message: types.Message):
-        if self.__isBotAdmin(bot, message.chat.id):
-            await self.__giveCaptcha(message)
+        if await self.__isBotAdmin(bot, message.chat.id):
+            await self.__giveCaptcha(bot, message)
 
     async def __isBotAdmin(self, bot: Bot, chat_id):
         bot_user: types.User = await bot.get_me()
         bot_id = bot_user.id
         bot_chat_member = await bot.get_chat_member(chat_id, bot_id)
-        return bot_chat_member.is_chat_admin()
+        return bot_chat_member.can_restrict_members
 
-    async def __giveCaptcha(self, message: types.Message):
+    async def __giveCaptcha(self, bot: Bot, message: types.Message):
+        bot_user: types.User = await bot.get_me()
+        if message.new_chat_members[0] == bot_user:
+            return
+
+        await bot.restrict_chat_member(message.chat.id,
+                                       message.from_user.id,
+                                       can_send_messages=False,
+                                       can_send_media_messages=False,
+                                       can_send_other_messages=False)
         markup = types.InlineKeyboardMarkup(resize_keyboard=True)
         item1 = types.InlineKeyboardButton("human", callback_data=CAPTCHA_SUCCESS)
         item2 = types.InlineKeyboardButton("I am clearly a spam bot", callback_data=CAPTCHA_SUCCESS)
