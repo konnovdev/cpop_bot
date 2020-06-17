78d8ab6042351bc5b57a695420d97122f11e9d1c
ixearth <35573806+ixearth@users.noreply.github.com>
2020-06-17 07:25:11 +0000

    YouTube Audio: add thumbnail
    Add patch files for aiogram and pytube3
    
    Update README.md

--- text_handler.py
+++ text_handler.py
@@ -4,11 +4,14 @@ import os
 from aiogram import types
 from aiogram.types import ParseMode
 from pytube import YouTube
+from urllib.request import urlopen
+from PIL import Image
 import config
 
 logger = logging.getLogger(__name__)
 
 
+# TODO move youtube logic into a separate class
 class TextHandler:
 
     async def handle(self, message: types.Message):
@@ -26,18 +29,48 @@ class TextHandler:
             author = yt.author
             length = yt.length
             title = yt.title
-            stream = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('bitrate').desc().first()
+            stream = (yt.streams
+                      .filter(only_audio=True, file_extension='mp4')
+                      .order_by('bitrate').desc().first())
             filesize = stream.filesize
             telegram_audio_limit = 52428800
+
             if (filesize < telegram_audio_limit and length < 600):
-                output_filename = stream.download("downloads")
-                with open(output_filename, "rb") as f:
-                    await message.reply_audio(f, duration=length, performer=author, title=title)
-                os.remove(output_filename)
+                output_audiofile = stream.download("downloads")
+                output_thumbnail = "downloads/square_thumbnail.jpg"
+                self.__make_squarethumb(yt.thumbnail_url, output_thumbnail)
+                with open(output_audiofile, "rb") as audio, \
+                     open(output_thumbnail, "rb") as thumb:
+                    await message.reply_audio(audio,
+                                              duration=length,
+                                              performer=author,
+                                              title=title,
+                                              thumb=thumb)
+                os.remove(output_audiofile)
+                os.remove(output_thumbnail)
             else:
-                await message.reply("<code>No downloads for 10min+ audio or file size greater than 50M</code>",
+                await message.reply("<code>No downloads for 10min+ audio" +
+                                    "or file size greater than 50M</code>",
                                     parse_mode=ParseMode.HTML)
         except Exception as e:
-            await message.reply("I've tried downloading this video but caught the "
-                                "following error: " + str(e) + ".\n\n<b>Please report it to @konnov</b>",
+            await message.reply("I've tried downloading this video but" +
+                                "caught the following error: <code>" +
+                                str(e) + "</code>.\n\n" +
+                                "<b>Please report it to @konnov</b>",
                                 parse_mode=ParseMode.HTML)
+
+    # https://stackoverflow.com/a/52177551
+    def __make_squarethumb(self, img_url, output):
+        original_thumb = Image.open(urlopen(img_url))
+        squarethumb = self.__crop_to_square(original_thumb)
+        squarethumb.thumbnail((320,320), Image.ANTIALIAS)
+        squarethumb.save(output)
+
+    def __crop_to_square(self, img):
+        width, height = img.size
+        length = min(width, height)
+        left = (width - length)/2
+        top = (height - length)/2
+        right = (width + length)/2
+        bottom = (height + length)/2
+        return img.crop((left, top, right, bottom))
