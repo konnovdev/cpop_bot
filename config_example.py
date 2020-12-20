47c187c3c8ad957f520a9500d7d735d957e1e2dc
Dash Eclipse <dashezup@disroot.org>
2020-12-20 13:38:05 +0000

    config_example.py: reduce indentation and other minor changes

--- config_example.py
+++ config_example.py
@@ -8,35 +8,35 @@ DEV_ID = 2011545269  # errors will be forwarded to this user ID
 PROMOTION_LIST_CHAT_ID = (
     -1001853277651,
     -1001748900794
-)
+    )
 
 WHITELIST_CHAT_ID = PROMOTION_LIST_CHAT_ID + (
     DEV_ID,
     8557378983
-)
+    )
 
 # Configure a response to `/help` message
 AVAILABILITY_HTML = ("This bot only serves cpop.tw group @mandopop and its "
                      "group members in private chat.")
 CONTACTS_HTML = ("Regarding any issues with the bot feel free to contact "
-                 "@konnov")
+                 "<a href=\"https://t.me/konnov\">ilya</a>")
 ABOUT_HTML = ("This bot is going to manage cpop.tw.\n"
               "For now it downloads music from YouTube or SoundCloud "
               "whenever you send the link and makes new members go through "
-              "captcha." + "\n\n"
-                           "<i>Source code available at cpop.tw/code under GPLv3 "
-                           "license</i>\n\n" +
+              "captcha.\n\n"
+              "<i>Source code available at cpop.tw/code under GPLv3 "
+              "license</i>\n\n" +
               CONTACTS_HTML)
-HELP_HTML = (AVAILABILITY_HTML + "\n\n"
-                                 "<b>Usage</b>:\n"
-                                 "- send a message that only contains a link of a YouTube video "
-                                 "or a SoundCloud track.\n"
-                                 "- Playlists are not supported, but you can select multiple "
-                                 "audio messages and forward them to create playlist instead.\n"
-                                 "- Your message will be deleted in private chat after the music "
-                                 "gets successfully uploaded.\n"
-                                 "- You can get YouTube links with inline bot @vid and use "
-                                 "them.\n\n" +
+HELP_HTML = (AVAILABILITY_HTML +
+             "\n\n<b>Usage</b>:\n"
+             "- send a message that only contains a link of a YouTube video "
+             "or a SoundCloud track.\n"
+             "- Playlists are not supported, but you can select multiple "
+             "audio messages and forward them to create playlist instead.\n"
+             "- Your message will be deleted in private chat after the music "
+             "gets successfully uploaded.\n"
+             "- You can get YouTube links with inline bot @vid and use "
+             "them.\n\n" +
              CONTACTS_HTML)
 
 # Music will be temporary downloaded to this destination
